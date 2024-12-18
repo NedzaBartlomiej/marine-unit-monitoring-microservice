19ed77aca981d5b02b9f8d4746ec943126c72af7
Bartlomiej <bartek21122006@gmail.com>
2024-12-18 21:35:30 +0100

    naming and lib ref refactoring

--- protection-service/src/main/java/pl/bartlomiej/protectionservice/iploginprotection/common/config/ProtectionAuthorizedRestClientConfig.java
+++ protection-service/src/main/java/pl/bartlomiej/protectionservice/iploginprotection/common/config/ProtectionAuthorizedRestClientConfig.java
@@ -1,17 +1,15 @@
 package pl.bartlomiej.protectionservice.iploginprotection.common.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
-import pl.bartlomiej.mumcommons.core.webtools.requestinterceptor.authorizedinterceptor.*;
+import pl.bartlomiej.mumcommons.core.webtools.requesthandler.authorizedhandler.servlet.*;
 
-@Slf4j
 @Configuration
-public class AuthorizedRestClientConfig {
+public class ProtectionAuthorizedRestClientConfig {
 
     @Bean
     RestClient ipLoginProtectionRestClient(@Qualifier("protectionAuthorizedRequestInterceptor") ClientHttpRequestInterceptor interceptor) {
